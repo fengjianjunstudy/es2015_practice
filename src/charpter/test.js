6a8b61e17d7d17a6560145c6d24cae705fa7b604
fjj <fjj_321@126.com>
2016-05-16 14:21:44 +0800

    babel  es6 to es5 with .map file

---
+++ src/charpter/test.js
@@ -0,0 +1,13 @@
+/**
+ * Created by fengjj on 2016/5/16.
+ */
+'use strict';
+require('babel-polyfill');
+let a = new Set();
+a.add('hello').add('word');
+console.log(a);
+
+
+
+
+
---
+++ src/charpter/transform-value.js
@@ -0,0 +1,7 @@
+/**
+ * Created by fengjj on 2016/5/10.
+ */
+"use strict";
+let [x,y] = [8,6];
+[x,y] = [y,x];
+console.log(x,y);
